3f3d21c7ad943a983d1529176d884b1b2a8502fa
dev2xl <contact@asosa.dev>
2023-09-28 18:08:34 -0400

    add sample client app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 
 // Routers
 import authRouter from "@/routes/auth";
@@ -14,5 +15,11 @@ app.use(express.urlencoded({ extended: false }));
 // Setup routes
 app.use("/api/auth", authRouter);
 app.use("/api/tracking-parcel", trackingRouter);
+
+// Static
+app.use(express.static(path.join(__dirname, "../public/")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 export default app;
