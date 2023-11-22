9082ee01d4d22a7660ee973c5a22c32f5b0fdcbd
Alberto Sosa <146108762+dev2xl@users.noreply.github.com>
2023-11-22 13:51:30 -0500

    Sign the token

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,11 +1,18 @@
 import jsonwebtoken from "jsonwebtoken";
 
 /**
- * Create a json web token combining the login+password
+ * Create a json web token
  * @param login
  * @param password
  */
 export const createAuthToken = (login: string, password: string) => {
-  const response = login + password;
-  return jsonwebtoken.sign(response, process.env.SECRET);
+
+  // find the user on DB with login, if exists then compare with bcrypt the password
+  // if it is all good sign the token and return it
+  if (!login || !password) {
+    return new Error("Invalid login or password")
+  }
+
+  return jsonwebtoken.sign(login, process.env.SECRET);
+
 };
